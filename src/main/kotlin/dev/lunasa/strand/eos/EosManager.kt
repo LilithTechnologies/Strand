bf64fba47e515c1ac50c2e9e3b6d4a2fe29beba9
LunasaVT <me@lunasa.dev>
2026-07-23 02:55:34 +0530

    Log EOS only on warn

--- src/main/kotlin/dev/lunasa/strand/eos/EosManager.kt
+++ src/main/kotlin/dev/lunasa/strand/eos/EosManager.kt
@@ -96,7 +96,7 @@ object EosManager {
             return
         }
         EosLogging.setCallback { msg -> onLog(msg.category, msg.level, msg.message) }
-        EosLogging.setLogLevel(EosLogCategory.AllCategories, EosLogLevel.VeryVerbose)
+        EosLogging.setLogLevel(EosLogCategory.AllCategories, EosLogLevel.Warning)
 
         current = createPlatform() ?: run {
             running = false
